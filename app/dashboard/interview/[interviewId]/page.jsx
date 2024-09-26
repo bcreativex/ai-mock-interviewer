4ec414855db78c55d5cc6de2ac64d39612b123c8
krishna <krishnajatav504@gmail.com>
2024-09-26 19:30:59 +0530

    interview start

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -25,9 +25,23 @@ function Interview({params}) {
     setInterviewData(result[0])
    }
   return (
-    <div className='my-10 flex justify-center flex-col items-center'>
+    <div className='my-10 '>
       <h2 className='font-bold text-2xl'>Let's Get Started</h2>
-      <div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+      
+
+        <div className='flex flex-col my-5 gap-5'>
+              <div className='flex flex-col p-5 rounded-lg border gap-5'>
+                  <h2 className='text-lg'><strong>Job Role/Job Position: </strong>{InterviewData.jobPosition}</h2>
+                  <h2 className='text-lg'><strong>Job Description/Tech Stack: </strong>{InterviewData.jobDesc}</h2>
+                  <h2 className='text-lg'><strong>Years of Experience: </strong>{InterviewData.jobExperience}</h2>
+              </div>
+              <div className='p-5 border rounded-lg border-yellow-300 bg-yellow-100'>
+                <h2 className='flex gap-2 items-center text-yellow-500'><Lightbulb/><strong>Information</strong></h2>
+                <h2 className='mt-3 text-yellow-500'>{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+              </div>
+        </div>
+        <div>
         {webCamEnabled? <Webcam
         onUserMedia={()=>setWebCamEnabled(true)}
         onUserMediaError={()=>setWebCamEnabled(false)}
@@ -40,19 +54,17 @@ function Interview({params}) {
         :
         <>
         <WebcamIcon className='h-72 w-full my-7 p-20 bg-secondary rounded-lg border'/>
-        <Button  onClick={()=>setWebCamEnabled(true)}>Enable WebCam and Microphone</Button>
+        <Button variant= 'ghost' className='w-full' onClick={()=>setWebCamEnabled(true)}>Enable WebCam and Microphone</Button>
         </>
         } 
         </div>
-
-        <div className='flex flex-col my-5 gap-5'>
-         
-              <h2 className='text-lg'><strong>Job Role/Job Position: </strong>{InterviewData.jobPosition}</h2>
-              <h2 className='text-lg'><strong>Job Description/Tech Stack: </strong>{InterviewData.jobDesc}</h2>
-              <h2 className='text-lg'><strong>Years of Experience: </strong>{InterviewData.jobExperience}</h2>
-        </div>
-          
+      </div>
+      
+          <div className='flex justify-end items-end'>
+          <Button>Start Interview</Button>    
+          </div>
     </div>
+
         
   )
 }
