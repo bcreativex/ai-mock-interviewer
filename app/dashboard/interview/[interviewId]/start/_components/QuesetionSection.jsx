59e3110c4fda8047f2a7109bd983b40a70427d83
krishna <krishnajatav504@gmail.com>
2024-09-28 00:40:50 +0530

    text to speech/ vice versa

--- app/dashboard/interview/[interviewId]/start/_components/QuesetionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuesetionSection.jsx
@@ -1,7 +1,20 @@
-import { Lightbulb } from 'lucide-react'
+import { Lightbulb, Volume2 } from 'lucide-react'
 import React from 'react'
 
 function QuesetionSection({mockInterviewQuestion,activeQuestionIndex}) {
+
+  const textToSpeech=(text)=>{
+    if('speechSynthesis' in window) {
+      const speech=new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech)
+    }
+    else{
+        alert('Sorry, Your browser does not support text to Speech')
+    }
+
+    }
+  
+
   return mockInterviewQuestion&& (
     <div className='p-5 border rounded-lg my-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
@@ -14,6 +27,8 @@ function QuesetionSection({mockInterviewQuestion,activeQuestionIndex}) {
             
         </div>
         <h2 className='my-5 text-md md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]?.question}</h2>
+       
+       <Volume2 className='cursor-pointer' onClick={()=>textToSpeech(mockInterviewQuestion[activeQuestionIndex]?.question)}/>
 
         <div className='border rounded-lg p-5 bg-blue-100 mt-20'>
           <h2 className='flex gap-2 items-center text-primary'>
