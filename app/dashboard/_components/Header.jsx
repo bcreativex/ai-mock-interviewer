88f7ea2c24c3db4733c446bd3310bc8843d183aa
krishna <krishnajatav504@gmail.com>
2024-10-01 01:51:47 +0530

    removed unnecessary

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,9 +14,9 @@ function Header() {
         <Image src={'/logo.svg'} width={160} height={100} alt='logo'/>
         <ul className='hidden md:flex gap-6'>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/question' && 'text-primary font-bold'}`}>Questions</li>
+            {/* <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/question' && 'text-primary font-bold'}`}>Questions</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li> */}
         </ul>
         <UserButton/>
     </div>
