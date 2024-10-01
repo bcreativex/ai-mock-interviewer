01bad5f573b1670ccb2bc4219f5e7afb5f5a02fa
krishna <krishnajatav504@gmail.com>
2024-10-01 15:44:31 +0530

    User button and dashboard rm

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -13,12 +13,12 @@ function Header() {
     <div className='flex p-4 items-center justify-between bg-secondary ow-sm'>
         <Image src={'/logo.png'} width={50} height={50} alt='logo'/> 
         <ul className='hidden md:flex gap-6'>
-            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
+            {/* <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li> */}
             {/* <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/question' && 'text-primary font-bold'}`}>Questions</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrade</li>
             <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li> */}
         </ul>
-        <UserButton/>
+        {/* <UserButton/> */}
     </div>
   )
 }
